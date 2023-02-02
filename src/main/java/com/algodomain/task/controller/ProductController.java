e7cc258b4ff0d6a09447b14679e3e6ec797ec842
Amol Nandivale <nandivaleamol@gmail.com>
2023-02-02 14:42:30 +0530

    Rest API's

--- src/main/java/com/algodomain/task/controller/ProductController.java
+++ src/main/java/com/algodomain/task/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/") ////since we’re just working locally
 @RequestMapping("/api/products")
 public class ProductController {
 
